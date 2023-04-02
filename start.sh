ad0574e1fbb9654aded78429c6b833409cb6224d
karan2144 <119487411+karan2144@users.noreply.github.com>
2023-04-02 18:05:51 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-echo "Cloning Repo...."
-git clone https://github.com/karan/Shortner-Converter-Bot-V2.git /Shortner-Converter-Bot-V2
-cd /Shortner-Converter-Bot-V2
-pip3 install -r requirements.txt
-echo "Starting Bot...."
-python3 bot.py
